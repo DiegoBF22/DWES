244616e5f954a1e0aa30ce06929d03a08bd93199
Diego <dbecerraf22@fpcoruna.afundacion.org>
2023-10-11 14:56:36 +0200

    Añadimos la opción de convertir pies a metros en conversor2.php

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,6 +9,11 @@
 					$v_pulgadas = $_POST["fcantidad"];
 					$v_metros = $v_pulgadas * 0.0254;
 					echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+				}
+				if ($_POST["funidad"] =="pie") {
+					$v_pie = $_POST["fcantidad"];
+					$v_metros = $v_pie * 0.3048;
+					echo $v_pie."pie(s) = ".$_metros." metro(s)";
 				} else {
 				  echo "Unidad no soportada";
 				}
@@ -22,6 +27,8 @@
 
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="pie_input" name="funidad" value="pie">
+			<label for="pie_input">Pie(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 
