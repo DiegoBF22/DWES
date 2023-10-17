e20ff24412a61103f7f1b772286ad2153f52f604
Diego <dbecerraf22@fpcoruna.afundacion.org>
2023-10-17 14:56:39 +0200

    primera actualización de la página

--- sprint2sql/www/mysite/main.php
+++ sprint2sql/www/mysite/main.php
@@ -1,4 +1,53 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<head>
+		<style>
+			thead{
+				font-weight: bolder;
+				font-size: 25px;
+				line-height: 50px;
+			}
+			tbody{
+				font-size: 18px;
+				line-height: 50px;
+			}
+		</style>
+	</head>
+	<body align='center'>
+	   <h1>Conexión establecida</h1>
+	   <table border='5'>
+		<thead>
+			<tr>
+			   <th>Nombre</th>
+			   <th>Imagen</th>
+			   <th>PEGI</th>
+			   <th>Lanzamiento</th>
+			</tr>
+		</thead>
+		<tbody align='center'>
+		   <?php
+			$query = 'SELECT * FROM tJuegos';
+			$result = mysqli_query($db, $query) or die('Query error');
+			while ($row = mysqli_fetch_array($result)) {
+			    	echo '<tr>';
+				echo '<td>';
+				echo $row[1];
+				echo '</td>';
+				echo '<td>';
+				echo '<img src= "';
+				echo $row[2];
+				echo '" width="150px">';
+				echo '</td>';
+				echo '<td>';
+				echo $row[3];
+				echo '</td>';
+				echo '<td>';
+				echo $row[4];
+				echo '</td>';
+			}
+	   	    ?>
+		</tbody>
+	</body>
+</html>
