d39c8cdcfaf87ac8661c83793ded83cd34658993
DiegoBF22 <145001059+DiegoBF22@users.noreply.github.com>
2023-10-31 13:01:04 +0100

    Ejercicio 3 (pero esta vez solo con las funciones)

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,5 +1,3 @@
-import math
-
 def suma(a,b):
     return a + b
 
@@ -15,31 +13,3 @@ def dividir(a,b):
     else:
         return a / b
 
-def menu():
-    
-    a = float(input("Ingresa el primer número: "))
-    b = float(input("Ingresa el segundo número: "))
-    
-    print("Selecciona la operación que desea hacer: ")
-    print("Sumar(S)")
-    print("Restar(R)")
-    print("Multiplicar(M)")
-    print("Dividir(D)")
-    respuesta = input("Elige una opción (S,R,M o D): ")
-
-    if respuesta.lower() == "s":
-        resultado = suma(a,b)
-    
-    if respuesta.lower() == "r":
-        resultado = resta(a,b)
-        
-    if respuesta.lower() == "m":
-        resultado = multiplicar(a,b)
-    
-    if respuesta.lower() == "d":
-        resultado = dividir(a,b)
-    
-    print("El resultado es: ", math.trunc(resultado))
-    
-if __name__ == "__main__":
-    menu()
